cc46304abaa50413388b8c6e916bd204bae8fdab
kotr228 <trockijkostantin@gmail.com>
2025-05-17 17:12:26 +0300

    coments

---
+++ src/pages/coments/coments.tsx
@@ -0,0 +1,19 @@
+import Header from '../../components/headermenu/header'
+import Footer from '../../components/footer/footer'
+import ComentsIn from '../../components/comentsin/comentsin';
+
+import React from 'react';
+
+const Coments: React.FC = () => {
+  return (
+    <div>
+      <Header />
+
+      <ComentsIn />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Coments;
