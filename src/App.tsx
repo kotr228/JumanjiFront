59cc52b2065dbb8fb1d9ba0d3c21562bd8c8eef5
kotr228 <trockijkostantin@gmail.com>
2025-05-26 19:58:42 +0300

    bilds

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import './App.css'
 import React, { useEffect } from 'react';
